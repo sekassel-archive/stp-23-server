85c8545a20b5480a54335ff45f8c2944fa1e23e8
Adrian Kunz <clashsoft@hotmail.com>
2023-03-01 17:19:11 +0100

    fix: Add mongoose module to encounter

--- src/encounter/encounter.module.ts
+++ src/encounter/encounter.module.ts
@@ -1,10 +1,20 @@
 import {Module} from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
 import {MonsterModule} from '../monster/monster.module';
 import {EncounterController} from './encounter.controller';
+import {Encounter, EncounterSchema} from './encounter.schema';
 import {EncounterService} from './encounter.service';
 
 @Module({
-  imports: [MonsterModule],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Encounter.name,
+        schema: EncounterSchema,
+      },
+    ]),
+    MonsterModule,
+  ],
   providers: [EncounterService],
   controllers: [EncounterController],
   exports: [EncounterService],
