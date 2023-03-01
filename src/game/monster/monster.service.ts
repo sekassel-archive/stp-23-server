7b3473f1ac0df90000af7b3b2a41928af1ca1bf8
Adrian Kunz <clashsoft@hotmail.com>
2023-03-01 17:23:39 +0100

    fix: Monster events

--- src/game/monster/monster.service.ts
+++ src/game/monster/monster.service.ts
@@ -38,6 +38,6 @@ export class MonsterService {
   }
 
   private emit(event: string, monster: Monster): void {
-    this.eventEmitter.emit(`regions.${monster.region}.monsters.${monster._id}.${event}`, monster);
+    this.eventEmitter.emit(`monsters.${monster._id}.${event}`, monster);
   }
 }
