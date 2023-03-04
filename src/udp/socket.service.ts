6d9d453617add192a5c4aeeef0d9f078303024c2
Adrian Kunz <clashsoft@hotmail.com>
2023-03-04 16:57:22 +0100

    feat: Socket pattern matching

--- src/udp/socket.service.ts
+++ src/udp/socket.service.ts
@@ -7,9 +7,16 @@ function key(rinfo: RemoteInfo) {
   return `${rinfo.address}:${rinfo.port}`;
 }
 
+function regex(pattern: string): RegExp {
+  if (!/^[\w.*]+$/g.test(pattern)) {
+    throw new Error(`Invalid event pattern: ${pattern}`);
+  }
+  return new RegExp(`^${pattern.replace(/\./g, '\\.').replace(/\*/g, '[^.]+')}$`);
+}
+
 interface Remote {
   info: RemoteInfo;
-  subscribed: string[];
+  subscribed: RegExp[];
 }
 
 @Injectable()
@@ -33,17 +40,18 @@ export class SocketService implements OnModuleInit {
     switch (message.event) {
       case 'subscribe': {
         const remote = this.remotes.get(key(rinfo));
+        const regExp = regex(message.data);
         if (remote) {
-          remote.subscribed.push(message.data);
+          remote.subscribed.push(regExp);
         } else {
-          this.remotes.set(key(rinfo), {info: rinfo, subscribed: [message.data]});
+          this.remotes.set(key(rinfo), {info: rinfo, subscribed: [regExp]});
         }
         break;
       }
       case 'unsubscribe': {
         const remote = this.remotes.get(key(rinfo));
         if (remote) {
-          remote.subscribed.splice(remote.subscribed.indexOf(message.data), 1);
+          remote.subscribed.splice(remote.subscribed.indexOf(regex(message.data)), 1);
           if (!remote.subscribed.length) {
             this.remotes.delete(key(rinfo));
           }
@@ -59,7 +67,9 @@ export class SocketService implements OnModuleInit {
   broadcast(event: string, data?: any): void {
     const message = JSON.stringify({event, data});
     for (const remote of this.remotes.values()) {
-      this.socket.send(message, remote.info.port, remote.info.address);
+      if (remote.subscribed.some(regExp => regExp.test(event))) {
+        this.socket.send(message, remote.info.port, remote.info.address);
+      }
     }
   }
 }
