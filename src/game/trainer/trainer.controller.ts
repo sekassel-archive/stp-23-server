7bc32f5e0871a3dcb1e2e9ad8bfde61405d7822c
Adrian Kunz <clashsoft@hotmail.com>
2023-03-04 13:29:35 +0100

    feat: Filter trainers by area

--- src/game/trainer/trainer.controller.ts
+++ src/game/trainer/trainer.controller.ts
@@ -1,8 +1,9 @@
-import {Controller, Get, Param} from '@nestjs/common';
-import {ApiOkResponse, ApiTags} from '@nestjs/swagger';
+import {Controller, Get, Param, Query} from '@nestjs/common';
+import {ApiOkResponse, ApiQuery, ApiTags} from '@nestjs/swagger';
 import {Auth} from '../../auth/auth.decorator';
 import {NotFound} from '../../util/not-found.decorator';
 import {ParseObjectIdPipe} from '../../util/parse-object-id.pipe';
+import {MONGO_ID_FORMAT} from '../../util/schema';
 import {Throttled} from '../../util/throttled.decorator';
 import {Validated} from '../../util/validated.decorator';
 import {Trainer} from './trainer.schema';
@@ -21,10 +22,12 @@ export class TrainerController {
 
   @Get()
   @ApiOkResponse({type: [Trainer]})
+  @ApiQuery({...MONGO_ID_FORMAT, name: 'area', required: false, description: 'Filter by area'})
   async findAll(
     @Param('regionId', ParseObjectIdPipe) regionId: string,
+    @Query('area', ParseObjectIdPipe) area?: string,
   ): Promise<Trainer[]> {
-    return this.trainerService.findAll(regionId);
+    return this.trainerService.findAll(regionId, {area});
   }
 
   @Get(':id')
