6cba0a69338b07d51b3ce2c064ae3b3dd66fa39f
Adrian Kunz <a.kunz@uni-kassel.de>
2023-03-26 11:24:17 +0200

    docs: Document talk target

--- src/game/trainer/trainer.dto.ts
+++ src/game/trainer/trainer.dto.ts
@@ -1,6 +1,7 @@
 import {ApiProperty, PickType} from '@nestjs/swagger';
 import {IsMongoId} from 'class-validator';
 import {PartialType} from '../../util/partial-type';
+import {MONGO_ID_FORMAT} from '../../util/schema';
 import {Trainer} from './trainer.schema';
 
 export class CreateTrainerDto extends PickType(Trainer, [
@@ -24,7 +25,12 @@ export class MoveTrainerDto extends PickType(Trainer, MOVE_TRAINER_PROPS) {
 }
 
 export class TalkTrainerDto extends PickType(Trainer, ['_id']) {
-  @ApiProperty()
+  @ApiProperty({
+    ...MONGO_ID_FORMAT,
+    description: 'The ID of the target trainer. ' +
+      'Talking to a Nurse (npc.canHeal) will heal all monsters. ' +
+      'Talking to a Trainer (npc.encounterOnSight) will start an encounter, even if that NPC already battled with you ("rematch").',
+  })
   @IsMongoId()
   target: string;
 }
