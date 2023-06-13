4523a3c7e4d235dc4d1fc814ee148cbad93c94a9
Adrian Kunz <clashsoft@hotmail.com>
2023-06-13 19:38:28 +0200

    dev: Add monster-summary.mjs

---
+++ assets/monster-summary.mjs
@@ -0,0 +1,52 @@
+const region = 'Albertania';
+import * as fs from 'node:fs/promises';
+import chalk from 'chalk';
+
+const foundMonsters = {};
+
+for (const file of await fs.readdir(`maps/${region}/`)) {
+  const area = file.slice(0, -5);
+  const map = JSON.parse(await fs.readFile(`maps/${region}/${file}`, 'utf8'));
+  for (const layer of map.layers) {
+    if (!layer.objects) {
+      continue;
+    }
+
+    for (const object of layer.objects) {
+      if (object.type !== 'Trainer' && object.type !== 'TallGrass') {
+        continue;
+      }
+
+      const monstersProp = object.properties?.find(p => p.name === 'Monsters')?.value;
+      if (monstersProp) {
+        const monsters = JSON.parse(monstersProp);
+        for (const [type, level] of monsters) {
+          (foundMonsters[type] ||= new Set).add(area);
+        }
+      }
+
+      const startersProp = object.properties?.find(p => p.name === 'Starters')?.value;
+      if (startersProp) {
+        const starters = JSON.parse(startersProp);
+        for (const type of starters) {
+          (foundMonsters[type] ||= new Set).add(area + ' (starter)');
+        }
+      }
+    }
+  }
+}
+
+const monsters = JSON.parse(await fs.readFile('monsters.json'));
+const baseMonsters = monsters.filter((m, index) => index === 0 || m.id !== monsters[index - 1].evolution);
+let found = 0;
+for (const monster of baseMonsters) {
+  const areas = foundMonsters[monster.id];
+  if (!areas || !areas.size) {
+    console.log(`#${monster.id} ${monster.name} ${chalk.red('not found')} in any area`);
+  } else {
+    found++;
+    console.log(`#${monster.id} ${monster.name} ${chalk.green('found')} in ${chalk.blue([...areas].join(', '))}`);
+  }
+}
+
+console.log(`Found ${found}/${baseMonsters.length} base monsters`);
