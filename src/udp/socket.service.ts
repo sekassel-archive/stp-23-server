e334e4805c1e99ba800cfb806cd2e86750619f75
Adrian Kunz <a.kunz@uni-kassel.de>
2023-06-16 10:24:26 +0200

    fix: Better UDP error handling
    Now invalid JSON messages are also handled.

--- src/udp/socket.service.ts
+++ src/udp/socket.service.ts
@@ -3,6 +3,7 @@ import {EventEmitter2} from '@nestjs/event-emitter';
 import {createSocket, RemoteInfo, Socket} from 'node:dgram';
 import {environment} from '../environment';
 import {SentryService} from "@ntegral/nestjs-sentry";
+import {Transaction} from "@sentry/node";
 
 function key(rinfo: RemoteInfo) {
   return `${rinfo.address}:${rinfo.port}`;
@@ -38,54 +39,57 @@ export class SocketService implements OnModuleInit {
   }
 
   async onMessage(msg: Buffer, info: RemoteInfo) {
-    const message = JSON.parse(msg.toString());
-    const tx = this.sentryService.instance().startTransaction({
-      op: 'udp',
-      name: message.event.replace(/[a-f0-9]{24}/g, '*'),
-      data: {
-        event: message.event,
-      },
-    });
-    switch (message.event) {
-      case 'subscribe': {
-        const remoteKey = key(info);
-        const remote = this.remotes.get(remoteKey);
-        const regExp = regex(message.data);
-        if (remote) {
-          remote.subscribed.set(message.data, regExp);
-        } else {
-          const subscribed = new Map<string, RegExp>;
-          subscribed.set(message.data, regExp);
-          this.remotes.set(remoteKey, {info, subscribed});
-        }
-        break;
-      }
-      case 'unsubscribe': {
-        const remoteKey = key(info);
-        const remote = this.remotes.get(remoteKey);
-        if (remote) {
-          remote.subscribed.delete(message.data);
-          if (!remote.subscribed.size) {
-            this.remotes.delete(remoteKey);
-          }
-        }
-        break;
-      }
-      default:
-        try {
-          await this.eventEmitter.emitAsync('udp:' + message.event, message.data);
-        } catch (e: any) {
-          if (e.response) {
-            tx.setStatus(HttpStatus[e.response.status]);
-            tx.setHttpStatus(e.response.status);
+    let message: { event: string, data?: any } = {event: 'unknown'};
+    let tx: Transaction | undefined;
+    try {
+      message = JSON.parse(msg.toString());
+      tx = this.sentryService.instance().startTransaction({
+        op: 'udp',
+        name: message.event.replace(/[a-f0-9]{24}/g, '*'),
+        data: {
+          event: message.event,
+        },
+      });
+      switch (message.event) {
+        case 'subscribe': {
+          const remoteKey = key(info);
+          const remote = this.remotes.get(remoteKey);
+          const regExp = regex(message.data);
+          if (remote) {
+            remote.subscribed.set(message.data, regExp);
           } else {
-            this.sentryService.error(e.message, e.stack, 'udp:' + message.event);
+            const subscribed = new Map<string, RegExp>;
+            subscribed.set(message.data, regExp);
+            this.remotes.set(remoteKey, {info, subscribed});
           }
-          this.socket.send(JSON.stringify({event: 'error', data: e.response || e.message}), info.port, info.address);
+          break;
         }
-        break;
+        case 'unsubscribe': {
+          const remoteKey = key(info);
+          const remote = this.remotes.get(remoteKey);
+          if (remote) {
+            remote.subscribed.delete(message.data);
+            if (!remote.subscribed.size) {
+              this.remotes.delete(remoteKey);
+            }
+          }
+          break;
+        }
+        default:
+          await this.eventEmitter.emitAsync('udp:' + message.event, message.data);
+          break;
+      }
+    } catch (e: any) {
+      if (e.response && tx) {
+        tx.setStatus(HttpStatus[e.response.status]);
+        tx.setHttpStatus(e.response.status);
+      } else {
+        this.sentryService.error(e.message, e.stack, 'udp:' + message.event);
+      }
+      this.socket.send(JSON.stringify({event: 'error', data: e.response || e.message}), info.port, info.address);
+    } finally {
+      tx?.finish();
     }
-    tx.finish();
   }
 
   broadcast(event: string, data?: any): void {
