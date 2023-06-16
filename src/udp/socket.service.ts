ccf774f0ebf18f3ce39d32209af936c628266bee
Adrian Kunz <a.kunz@uni-kassel.de>
2023-06-16 10:28:00 +0200

    refactor: Helpers in SocketService

--- src/udp/socket.service.ts
+++ src/udp/socket.service.ts
@@ -21,6 +21,11 @@ interface Remote {
   subscribed: Map<string, RegExp>;
 }
 
+interface Message {
+  event: string;
+  data?: any;
+}
+
 @Injectable()
 export class SocketService implements OnModuleInit {
   remotes = new Map<string, Remote>;
@@ -39,7 +44,7 @@ export class SocketService implements OnModuleInit {
   }
 
   async onMessage(msg: Buffer, info: RemoteInfo) {
-    let message: { event: string, data?: any } = {event: 'unknown'};
+    let message: Message = {event: 'unknown'};
     let tx: Transaction | undefined;
     try {
       message = JSON.parse(msg.toString());
@@ -50,35 +55,7 @@ export class SocketService implements OnModuleInit {
           event: message.event,
         },
       });
-      switch (message.event) {
-        case 'subscribe': {
-          const remoteKey = key(info);
-          const remote = this.remotes.get(remoteKey);
-          const regExp = regex(message.data);
-          if (remote) {
-            remote.subscribed.set(message.data, regExp);
-          } else {
-            const subscribed = new Map<string, RegExp>;
-            subscribed.set(message.data, regExp);
-            this.remotes.set(remoteKey, {info, subscribed});
-          }
-          break;
-        }
-        case 'unsubscribe': {
-          const remoteKey = key(info);
-          const remote = this.remotes.get(remoteKey);
-          if (remote) {
-            remote.subscribed.delete(message.data);
-            if (!remote.subscribed.size) {
-              this.remotes.delete(remoteKey);
-            }
-          }
-          break;
-        }
-        default:
-          await this.eventEmitter.emitAsync('udp:' + message.event, message.data);
-          break;
-      }
+      await this.onEvent(info, message);
     } catch (e: any) {
       if (e.response && tx) {
         tx.setStatus(HttpStatus[e.response.status]);
@@ -89,6 +66,41 @@ export class SocketService implements OnModuleInit {
       this.socket.send(JSON.stringify({event: 'error', data: e.response || e.message}), info.port, info.address);
     } finally {
       tx?.finish();
+    }
+  }
+
+  private async onEvent(info: RemoteInfo, message: Message): Promise<unknown> {
+    switch (message.event) {
+      case 'subscribe':
+        return this.subscribe(info, message.data);
+      case 'unsubscribe':
+        return this.unsubscribe(info, message.data);
+      default:
+        return this.eventEmitter.emitAsync('udp:' + message.event, message.data);
+    }
+  }
+
+  private subscribe(info: RemoteInfo, pattern: string) {
+    const remoteKey = key(info);
+    const remote = this.remotes.get(remoteKey);
+    const regExp = regex(pattern);
+    if (remote) {
+      remote.subscribed.set(pattern, regExp);
+    } else {
+      const subscribed = new Map<string, RegExp>;
+      subscribed.set(pattern, regExp);
+      this.remotes.set(remoteKey, {info, subscribed});
+    }
+  }
+
+  private unsubscribe(info: RemoteInfo, pattern: string) {
+    const remoteKey = key(info);
+    const remote = this.remotes.get(remoteKey);
+    if (remote) {
+      remote.subscribed.delete(pattern);
+      if (!remote.subscribed.size) {
+        this.remotes.delete(remoteKey);
+      }
     }
   }
 
