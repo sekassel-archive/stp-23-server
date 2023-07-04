2055250911756f97d2900e643f3239aa343e61bf
Adrian Kunz <a.kunz@uni-kassel.de>
2023-07-04 16:16:24 +0200

    fix: Log number of deleted opponents

--- src/game/opponent/opponent.scheduler.ts
+++ src/game/opponent/opponent.scheduler.ts
@@ -1,10 +1,12 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, Logger} from "@nestjs/common";
 import {Cron, CronExpression} from "@nestjs/schedule";
 import {environment} from "../../environment";
 import {OpponentService} from "./opponent.service";
 
 @Injectable()
 export class OpponentScheduler {
+  private logger = new Logger(OpponentScheduler.name);
+
   constructor(
     private opponentService: OpponentService,
   ) {
@@ -13,6 +15,7 @@ export class OpponentScheduler {
   @Cron(CronExpression.EVERY_5_MINUTES)
   async deleteStaleOpponents() {
     const beforeDate = new Date(Date.now() - 1000 * 60 * 60 * environment.cleanup.opponentLifetimeMinutes);
-    await this.opponentService.deleteMany({updatedAt: {$lt: beforeDate}});
+    const result = await this.opponentService.deleteMany({updatedAt: {$lt: beforeDate}});
+    result.deletedCount && this.logger.warn(`Deleted ${result.deletedCount} stale opponents`);
   }
 }
