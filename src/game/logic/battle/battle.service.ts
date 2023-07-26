8a45475d2f1d7e4d29ff5cad6bdced7857b9e5cc
Adrian Kunz <a.kunz@uni-kassel.de>
2023-07-26 11:00:38 +0200

    feat: Receive gifts after encounter defeat

--- src/game/logic/battle/battle.service.ts
+++ src/game/logic/battle/battle.service.ts
@@ -88,6 +88,7 @@ export class BattleService {
 
     const deletedOpponents = await this.deleteOpponentsWithoutMonsters(opponents);
     await this.markDefeatedNPCsAsEncounteredPlayer(opponents, deletedOpponents);
+    await this.giveRewards(opponents, deletedOpponents);
   }
 
   private async markDefeatedNPCsAsEncounteredPlayer(opponents: OpponentDocument[], deleteOpponents: OpponentDocument[]) {
@@ -101,6 +102,23 @@ export class BattleService {
         'npc.encountered': {$each: playerIds},
       },
     });
+  }
+
+  private async giveRewards(opponents: OpponentDocument[], deleteOpponents: OpponentDocument[]) {
+    const rewards = (await this.trainerService.findAll({
+      _id: {$in: deleteOpponents.map(o => new Types.ObjectId(o.trainer))},
+      'npc.gifts': {$exists: true},
+    }, {projection: {'npc.gifts': 1}})).map(t => t.npc?.gifts || []).flat();
+    if (!rewards.length) {
+      return;
+    }
+
+    const playerIds = opponents.filter(o => !o.isNPC && !deleteOpponents.includes(o)).map(o => o.trainer);
+    await Promise.all(playerIds.map(async playerId => {
+      return Promise.all(rewards.map(async reward => {
+        return this.itemService.updateAmount(playerId, reward, 1);
+      }));
+    }));
   }
 
   private async deleteOpponentsWithoutMonsters(opponents: OpponentDocument[]): Promise<OpponentDocument[]> {
